775b67329264f848aa5d218e16a76d8a730c4589
Debayan mukherjee <120113934+uxRakhal@users.noreply.github.com>
2025-10-21 22:02:17 +0530

    minor chngs

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'next/link';
+
 import AddToCartButton from '@/components/cart/AddToCartButton';
-import "@/styles/productCard.scss";
+import "@/styles/featuredProductCard.scss";
 
 function ProductCard() {
 
     const products = [
-    { id: "p1", title: "Snake Plant", price: 499 },
-    { id: "p2", title: "Peace Lily", price: 699 },
-    { id: "p3", title: "Areca Palm", price: 1199 },
-    { id: "p4", title: "ZZ Plant", price: 899 },
+    { id: "p1", image:"/indoor.jpg", title: "Snake Plant", price: 499 },
+    { id: "p2", image:"/outdoor.jpg", title: "Peace Lily", price: 699 },
+    { id: "p3", image:"/flowering.jpg", title: "Areca Palm", price: 1199 },
+    { id: "p4", image:"/herbs.jpg", title: "ZZ Plant", price: 899 },  
   ];
   return (
       <>
@@ -17,14 +18,12 @@ function ProductCard() {
           <div className="product-card" key={product.id}>
             <Link href={`/product/${product.slug}`}>
               <div className="product-image">
-                {/* Uncomment when images are ready */}
-                {/* <Image
-                  src={product.image}
-                  alt={product.name}
-                  width={200}
-                  height={200}
+                <img src={product.image}
+                  alt={product.title}
+                  width={280}
+                  height={300}
                   loading="lazy"
-                /> */}
+                />
               </div>
               <h3>{product.title}</h3>
               <p className="price">₹{product.price}</p>
